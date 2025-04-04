593c202f55e9d42f3ab358e62d03f305153ffc28
Varun17mk <kulkarnivarun10@gmail.com>
2025-04-04 15:52:24 +0530

    3rd commit

---
+++ src/StringBasedPrgms/StringSwap.java
@@ -0,0 +1,19 @@
+package StringBasedPrgms;
+
+public class StringSwap {
+    public static void main(String[] args) {
+        String a = "Good";
+        String b = "morning";
+
+        System.out.println("Before swapping...");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+
+        //1. append a and b.
+        a=a+b; //"Goodmorning"
+
+        //2. Store initial string a in string b
+
+
+    }
+}
