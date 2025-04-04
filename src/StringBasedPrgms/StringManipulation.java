1450fa5ceae0aba9c895fed99867ed117f6f513a
Varun17mk <kulkarnivarun10@gmail.com>
2025-04-04 14:07:46 +0530

    Strings prgs

--- src/StringBasedPrgms/StringManipulation.java
+++ src/StringBasedPrgms/StringManipulation.java
@@ -30,5 +30,40 @@ public class StringManipulation {
         System.out.println(str.equalsIgnoreCase(str1));
 
         //SubString
+        System.out.println(str.substring(0,9));
+
+        //trim
+       String str2 = "   Hey GULBARGA  ";
+        System.out.println(str2.trim());
+
+        //replace
+        System.out.println(str2.replace(" ",""));  // replace white spaces into nospaces
+
+        String date = "17/10/2000";
+        System.out.println(date.replace("/","-"));  // replace "/" into "-"
+
+        //split
+
+        String test = "Hello_World_Test_Selenium";
+        String testValue[] = test.split("_");
+        // to print array use for loop
+        for (String testArr : testValue) {
+            System.out.println(testArr);
+        }
+
+
+        //concatenate  Important for interview
+        String x = "Hey";
+        String y = "Varun";
+        int a = 100;
+        int b = 200;
+
+        System.out.println(x+y); //"HeyVarun"
+        System.out.println(a+b); //300
+        System.out.println(x+y+a+b); //"HeyVarun100200"
+        System.out.println(a+b+x+y); // "300HeyVarun"
+        System.out.println(x+y+(a+b)); //"HeyVarun300"
+
+
     }
 }
